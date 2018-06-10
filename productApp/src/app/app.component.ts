b58ccb5b2cbdbc7cbd104bbcee9b128fd2443a4b
AAKASH HANDA <AAKASH HANDA>
2018-06-10 20:17:42 +0530

    adding comp

---
+++ productApp/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-comp',
+    template:`<div>
+                <h1>This is main Component of ang 5</h1>
+                <book-comp></book-comp>
+              </div>`
+})
+
+export class AppComponent{
+
+}
